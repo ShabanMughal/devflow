35f85ab0b62692571126b751e48ff866785d788c
ShabanMughal <thandermughal@gmail.com>
2025-03-22 15:57:52 +0500

    Add navbar component

---
+++ components/navigation/index.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between background-light900_drk200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12
+    shadow-light-300'>
+      <Link href={'/'} className='flex items-center gap-1'>
+      <Image src='/images/site-logo.svg' alt='Dev Overflow' width={32} height={32} />
+      <p className='h2-bold font-space-grotesk text-dark-100 dark:text-light-900 
+      max-sm:hidden'>Dev<span className='text-primary-500'>Flow</span></p>
+      </Link>
+
+      <p>Global search</p>
+
+      <div className='flex-between gap-5'>
+        Theme
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
