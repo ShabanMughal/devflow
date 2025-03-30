c31ec5fe64f0cc9ce11dca40a0e887f713c036f7
ShabanMughal <thandermughal@gmail.com>
2025-03-30 11:35:53 +0500

    feat: craete form with validation

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AurhForm";
+import { SignInSchema } from "@/lib/validation";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return(
+    <AuthForm
+          formType="SIGN_UP"
+          schema={SignInSchema}
+          defaultValues={{  username: "", email: "", password: "" }}
+          onSubmit={(data) => Promise.resolve({ success: true, data })}
+        />
+  );
 };
 
 export default SignUp;
