f3084e1a8ae1dd1c5c9c25fa79b6de13ecd71cf1
ShabanMughal <thandermughal@gmail.com>
2025-04-02 20:28:59 +0500

    fix: resolve conflicts

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import AuthForm from "@/components/forms/AurhForm";
-import { SignInSchema } from "@/lib/validation";
+import { SignUpSchema } from "@/lib/validation";
 import React from "react";
 
 const SignUp = () => {
   return(
     <AuthForm
           formType="SIGN_UP"
-          schema={SignInSchema}
-          defaultValues={{  username: "", email: "", password: "" }}
+          schema={SignUpSchema}
+          defaultValues={{ username: "", email: "", password: "" }}
           onSubmit={(data) => Promise.resolve({ success: true, data })}
         />
   );
