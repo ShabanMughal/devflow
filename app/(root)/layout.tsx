b68a0a28101a76a3be11d2b7a2fe90de16503e5e
ShabanMughal <thandermughal@gmail.com>
2025-04-02 19:58:02 +0500

    feat: create leftsider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,20 @@
 import { ReactNode } from "react";
 
 import Navbar from "@/components/navigation/navbar";
+import LeftsideBar from "@/components/navigation/LeftSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
+      <div className="flex ">
+        <LeftsideBar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">
       {children}
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
