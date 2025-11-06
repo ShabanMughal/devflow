ef29ad722f27d7e2129df66a06c4ccbb2a791758
ShabanMughal <thandermughal@gmail.com>
2025-11-07 03:37:17 +0500

    fix: correct import path for LocalSearch component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
-import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import { api } from "@/lib/api";
 import handleError from "@/lib/handlers/error";
 import { auth } from "@/auth";
+import LocalSearch from "@/components/Search/LocalSearch";
 
 const questions = [
   {
