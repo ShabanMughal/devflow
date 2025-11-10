eb93ae961082890dd53294478a64c516b6027693
ShabanMughal <thandermughal@gmail.com>
2025-11-11 03:09:15 +0500

    feat: implement question creation with validation and routing, add question details page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,23 @@
-import QuestionForm from '@/components/forms/QuestionForm'
-import React from 'react'
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const AskQuestion = async () => {
+  const session = await auth();
+
+  if (!session) return redirect("/sign-in");
+
   return (
     <>
-     <h1 className='h1-bold text-dark100_light900'>
-        Ask a question
-        </h1> 
-    <div className='mt-9'>
-      <QuestionForm />
-    </div>
-    </>
-  )
-}
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
-export default page
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
+};
+
+export default AskQuestion;
